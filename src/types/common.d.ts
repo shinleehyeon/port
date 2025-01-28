58efc1b6a0014a03c4e40b7d98a90b740d6db634
shinleehyeon <shinlee7878@gmail.com>
2025-01-28 22:17:07 +0900

    add : project detail

--- src/types/common.d.ts
+++ src/types/common.d.ts
@@ -1,25 +1,38 @@
+import { IconType } from 'react-icons';
+
 export interface NavItem {
-    title: string;
-    path: string;
-  }
-  
-  export interface Skill {
-    icon: string;
-    desc: string;
-  }
-  
-  export interface Project {
-    id: number;
-    title: string;
-    description: string;
-    image: string;
-    tag: string[];
-    gitUrl: string;
-    techStack: IconType[];
-  }
-  
-  export interface Strength {
-    keyword: string;
-    detail: string;
-    color: string;
-  }
+  title: string;
+  path: string;
+}
+
+export interface Skill {
+  icon: string;
+  desc: string;
+}
+
+export interface TechDescription {
+  tech: string;
+  icon: IconType;
+}
+
+export interface Project {
+  id: number;
+  title: string;
+  description: string;
+  image: string;
+  tag: string[];
+  gitUrl: string;
+  techStack: IconType[];
+  details: ProjectDetails;
+}
+
+export interface Project {
+  id: number;
+  title: string;
+  description: string;
+  image: string;
+  tag: string[];
+  gitUrl: string;
+  techStack: IconType[];
+  details: ProjectDetails;
+}
