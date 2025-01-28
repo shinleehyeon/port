b9645d8074c3e913e05e2e695581b4d384710702
shinleehyeon <shinlee7878@gmail.com>
2025-01-28 14:34:08 +0900

    feat : icon add

--- src/lib/constants/projects.ts
+++ src/lib/constants/projects.ts
@@ -9,7 +9,10 @@ import {
   SiPython,
   SiNodemon,
   SiExpress,
-  SiPwa
+  SiPwa,
+  SiWebpack,
+  SiFastapi,
+  SiTypescript
 } from "react-icons/si";
 import type { Project } from '@/types/common';
 
@@ -21,7 +24,7 @@ export const projectsData: Project[] = [
     image: "/images/projects/fresio.png",
     tag: ["All", "Mobile"],
     gitUrl: "https://github.com/shinleehyun/fresio",
-    techStack: [FaReact, SiPython]
+    techStack: [FaReact, SiFastapi, SiTypescript]
   },
   {
     id: 2,
@@ -30,7 +33,7 @@ export const projectsData: Project[] = [
     image: "/images/projects/SaveQuest.png",
     tag: ["All", "Mobile"],
     gitUrl: "https://github.com/SaveQuest",
-    techStack: [FaReact, SiNestjs]
+    techStack: [FaReact, SiNestjs, SiTypescript]
   },
   {
     id: 3,
@@ -39,7 +42,7 @@ export const projectsData: Project[] = [
     image: "/images/projects/Chromate.png",
     tag: ["All", "Web"],
     gitUrl: "https://github.com/shinleehyeon/chromate",
-    techStack: [FaReact, SiTailwindcss]
+    techStack: [FaReact, SiTailwindcss, SiFastapi]
   },
   {
     id: 4,
@@ -48,7 +51,7 @@ export const projectsData: Project[] = [
     image: "/images/projects/Albant.png",
     tag: ["All", "Mobile"],
     gitUrl: "https://github.com/shinleehyun/albant",
-    techStack: [FaReact, SiPython]
+    techStack: [FaReact, SiFastapi]
   },
   {
     id: 7,
@@ -57,6 +60,6 @@ export const projectsData: Project[] = [
     image: "/images/projects/port.png",
     tag: ["All", "Web"],
     gitUrl: "hhttps://github.com/shinleehyeon/port",
-    techStack: [SiNextdotjs, SiTailwindcss]
+    techStack: [SiNextdotjs, SiTailwindcss, SiTypescript]
   }
 ]
