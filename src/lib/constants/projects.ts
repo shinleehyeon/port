244e166458aa7003838a37e1c2371ffb54cf6498
shinleehyeon <shinlee7878@gmail.com>
2025-02-11 17:11:10 +0900

    fix : id

--- src/lib/constants/projects.ts
+++ src/lib/constants/projects.ts
@@ -16,6 +16,19 @@ export const projectsData: Project[] = [
   },
   {
     id: 2,
+    title: "Speakit",
+    description: "발표를 압도적으로 편안하게",
+    image: "/images/projects/Speakit.png",
+    tag: ["All", "Mobile"],
+    gitUrl: "https://github.com/shinleehyeon/Speakit",
+    techStack: [
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"
+    ]
+  },
+  {
+    id: 3,
     title: "SaveQuest",
     description: "절약을 쉽고 재미있게 하도록 도와주는 서비스",
     image: "/images/projects/SaveQuest.png",
@@ -28,7 +41,7 @@ export const projectsData: Project[] = [
     ]
   },
   {
-    id: 3,
+    id: 4,
     title: "Chromate",
     description: "지체 장애인을 위한 AI 음성 확장 프로그램",
     image: "/images/projects/Chromate.png",
@@ -41,7 +54,7 @@ export const projectsData: Project[] = [
     ]
   },
   {
-    id: 4,
+    id: 5,
     title: "Albant",
     description: "선린 내에서 이룰 수 있는 심부름 서비스",
     image: "/images/projects/Albant.png",
@@ -53,7 +66,7 @@ export const projectsData: Project[] = [
     ]
   },
   {
-    id: 5,
+    id: 6,
     title: "PortFolio",
     description: "포트폴리오 사이트",
     image: "/images/projects/port.png",
@@ -62,19 +75,6 @@ export const projectsData: Project[] = [
     techStack: [
       "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg",
       "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original.svg",
-      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"
-    ]
-  },
-  {
-    id: 6,
-    title: "Speakit",
-    description: "발표를 압도적으로 편안하게",
-    image: "/images/projects/Speakit.png",
-    tag: ["All", "Mobile"],
-    gitUrl: "https://github.com/shinleehyeon/Speakit",
-    techStack: [
-      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
-      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original.svg",
       "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"
     ]
   }
