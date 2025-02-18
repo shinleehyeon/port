2a719faf272317955577d327a15617b18597aae6
shinleehyeon <shinlee7878@gmail.com>
2025-02-19 00:42:39 +0900

    fix : about png

--- src/components/sections/about/AboutSection.tsx
+++ src/components/sections/about/AboutSection.tsx
@@ -210,7 +210,7 @@ const AboutSection: React.FC = () => {
             <div className="md:grid md:grid-cols-2 items-start gap-10 relative">
                 <div className="relative w-[300px] h-[400px]">
                     <Image
-                        src="/images/about1.png"
+                        src="/images/about.png"
                         alt="Hero image"
                         fill
                         priority
