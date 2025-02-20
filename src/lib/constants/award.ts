c750ab5e7da56bacee674bb64f98c989eb1799fd
shinleehyeon <shinlee7878@gmail.com>
2025-02-21 01:11:22 +0900

    add : award.ts

---
+++ src/lib/constants/award.ts
@@ -0,0 +1,31 @@
+import type { Award } from '@/types/common';
+
+export const awardsData: Award[] = [
+    {
+        titleKo: "SW 동행 해커톤",
+        titleEn: "SW Hackathon",
+        year: "2024",
+        organizationKo: "과학기술정보통신부",
+        organizationEn: "Ministry of Science and ICT",
+        typeKo: "한국 과학 창의재단 이사장상",
+        typeEn: "Korea Foundation for Science and Creativity Chairman's Award"
+    },
+    {
+        titleKo: "29th 앱잼",
+        titleEn: "29th AppJam",
+        year: "2025",
+        organizationKo: "SK플래닛",
+        organizationEn: "SK Planet",
+        typeKo: "최우수상",
+        typeEn: "Grand Prize"
+    },
+    {
+        titleKo: "스마틴 앱챌린지 (STAC)",
+        titleEn: "Smart Teen App Challenge (STAC)",
+        year: "2024",
+        organizationKo: "SK플래닛",
+        organizationEn: "SK Planet",
+        typeKo: "가작상",
+        typeEn: "Honorable Mention"
+    },
+];
