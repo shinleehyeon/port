2699608c9d8ec4f4d67460b841173ba7102d9753
shinleehyeon <shinlee7878@gmail.com>
2025-03-06 11:15:26 +0900

    feat: enhance SEO with improved metadata in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,24 @@ export const metadata: Metadata = {
   verification: {
     google: 'U3XR0SbCLhek7REd-BeocPYDC8CP7GqVe_elyQn4GLg',
   },
+  openGraph: {
+    type: 'website',
+    title: '신이현 | 개발자 포트폴리오',
+    description: '성장하는 개발자 신이현의 공식 포트폴리오 웹사이트입니다.',
+    url: 'https://www.2hyundev.com/',
+    siteName: '신이현 포트폴리오',
+    images: [
+      {
+        url: 'https://www.2hyundev.com/images/about.png',
+        width: 800,
+        height: 600,
+        alt: '신이현 포트폴리오',
+      },
+    ],
+  },
+  alternates: {
+    canonical: 'https://www.2hyundev.com/'
+  }
 };
 
 interface RootLayoutProps {
@@ -30,6 +48,30 @@ export default function RootLayout({ children }: RootLayoutProps) {
               gtag('js', new Date());
               gtag('config', 'G-JZZ3PWV3FP');
             `,
+          }}
+        />
+
+        <link rel="me" href="https://www.instagram.com/hyun._.s08/" />
+
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: `{
+              "@context": "https://schema.org",
+              "@type": "Person",
+              "name": "신이현",
+              "url": "https://www.2hyundev.com/",
+              "image": "https://www.2hyundev.com/images/about.png",
+              "jobTitle": "Software Developer",
+              "worksFor": {
+                "@type": "Organization",
+                "name": "선린인터넷고등학교"
+              },
+              "sameAs": [
+                "https://www.instagram.com/hyun._.s08/",
+                "https://github.com/shinleehyeon"
+              ]
+            }`
           }}
         />
       </head>
